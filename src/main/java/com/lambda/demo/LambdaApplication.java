51a771eb69d837ee87eaffcc9095865c314b13ca
Raffaele <raffaelecoppola2828@gmail.com>
2024-12-02 08:57:33 +0100

    First commit try

--- src/main/java/com/lambda/demo/LambdaApplication.java
+++ src/main/java/com/lambda/demo/LambdaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LambdaApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaApplication.class, args);
 	}
